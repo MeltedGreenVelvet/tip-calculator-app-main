7d64102896c46d8c4584a050e94f6c0bdae38e65
MeltedGreenVelvet <89157503+MeltedGreenVelvet@users.noreply.github.com>
2023-05-16 13:29:03 -0400

    Input Style & Functionality
    - Tweaked the custom button styling.
    - Modified JavaScript to account for custom button value.
    - Added active states to inputs.
    - Refactored the script.js to reduce redundant code caused by the custom button functionality.

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ const billInput = document.getElementById('bill');
 const numberOfPeopleInput = document.getElementById('people');
 const tipAmountInput = document.getElementById('tip-amount');
 const totalAmountInput = document.getElementById('total');
+const customInput = document.getElementById('custom-input');
 
 tipButtons.forEach(button => {
   button.addEventListener('click', () => {
-    const radioBtn = button.previousElementSibling;
+    const radioBtn = button.querySelector('input[type="radio"]');
     if (radioBtn) {
       radioBtn.checked = true;
+      calculateTip();
+    } else if (button === customInput.parentElement) {
       calculateTip();
     }
   });
@@ -37,26 +40,46 @@ radioButtons.forEach((radioButton, index) => {
 function calculateTip() {
   const billValue = parseFloat(billInput.value);
   const numberOfPeopleValue = parseInt(numberOfPeopleInput.value);
+  const customTipPercentage = parseFloat(customInput.value);
   const selectedTipButton = document.querySelector('input[type="radio"]:checked');
 
-  if (!isNaN(billValue) && !isNaN(numberOfPeopleValue) && selectedTipButton) {
-    const tipPercentage = parseFloat(selectedTipButton.value);
-    const tipAmount = (billValue * tipPercentage) / numberOfPeopleValue;
-    const totalAmount = billValue * tipPercentage;
-    const totalPerPerson = (billValue + totalAmount) / numberOfPeopleValue;
+  let tipPercentage, tipAmount, totalAmount, totalPerPerson;
 
-    tipAmountInput.value = tipAmount.toFixed(2);
-    totalAmountInput.value = totalPerPerson.toFixed(2);
+  if (!isNaN(billValue) && !isNaN(numberOfPeopleValue)) {
+    if (selectedTipButton) {
+      tipPercentage = parseFloat(selectedTipButton.value);
+    } else if (!isNaN(customTipPercentage)) {
+      tipPercentage = customTipPercentage * 0.01; // Convert to percentage
+    }
+
+    if (tipPercentage) {
+      tipAmount = (billValue * tipPercentage) / numberOfPeopleValue;
+      totalAmount = billValue * tipPercentage;
+      totalPerPerson = (billValue + totalAmount) / numberOfPeopleValue;
+
+      tipAmountInput.value = tipAmount.toFixed(2);
+      totalAmountInput.value = totalPerPerson.toFixed(2);
+    }
   }
 }
 
+
 billInput.addEventListener('input', calculateTip);
 numberOfPeopleInput.addEventListener('input', calculateTip);
+customInput.addEventListener('input', () => {
+  radioButtons.forEach(radioButton => {
+    radioButton.checked = false;
+    const label = radioButton.parentNode;
+    label.classList.remove('checked');
+  });
+  calculateTip();
+});
 
 resetButton.addEventListener('click', () => {
   tipButtons.forEach(tipButton => {
     tipButton.classList.remove('checked');
   });
+  customInput.value = '';
   tipAmountInput.value = '';
   totalAmountInput.value = '';
 });
